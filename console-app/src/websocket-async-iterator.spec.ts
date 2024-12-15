26b66f3fc291877e1d08775fcf568aacd7f36d5c
Nikolay Chorniy <nikolay.chorniy@ehawksolutions.com>
2024-12-15 18:53:43 +0000

    tests

--- console-app/src/websocket-async-iterator.spec.ts
+++ console-app/src/websocket-async-iterator.spec.ts
@@ -3,6 +3,7 @@ import {subscribeToOptions} from "./subscribe-to-stream";
 import {toZonedTime} from "date-fns-tz";
 import {addDays} from "date-fns";
 import WebSocket from "ws";
+import {dateToLocalString} from "./date-utils";
 
 const closedCall = vi.fn<WebSocket['close']>()
 vi.mock('ws', async (importOriginal) => {
@@ -17,9 +18,28 @@ vi.mock('ws', async (importOriginal) => {
 })
 describe("websocket-async-iterator", () => {
     it('should be possible to stop iteration', async () => {
-        const {iterator, stop} = subscribeToOptions({maturityDates: [addDays(toZonedTime(Date.now(), 'UTC'), 1)]})
+        const date = addDays(toZonedTime(Date.now(), 'UTC'), 1);
+        const expectedDateString = dateToLocalString(date)
+        const {iterator, stop} = subscribeToOptions({maturityDates: [date]})
         let index = 0
         for await (const item of iterator()) {
+            if (Array.isArray(item)) {
+                expect(item.length).toBeTruthy()
+                item.forEach((i) => {
+                    expect(i.maturityDate).toStrictEqual(expectedDateString)
+                    expect(i.tradingPair).toBe("BTC-USDT")
+                    expect(typeof i.eventTime).toBe('number')
+                    expect(typeof i.transactionTime).toBe('number')
+                    expect(typeof i.bestBuyPrice).toBe('number')
+                    expect(typeof i.bestBuyQuantity).toBe('number')
+                    expect(typeof i.bestCellPrice).toBe('number')
+                    expect(typeof i.bestCellQuantity).toBe('number')
+                    expect(typeof i.theta).toBe('number')
+                    expect(typeof i.gamma).toBe('number')
+                    expect(typeof i.vega).toBe('number')
+                    expect(typeof i.delta).toBe('number')
+                })
+            }
             console.log("Received", item)
             if (++index === 2) {
                 console.log("Stopping")
